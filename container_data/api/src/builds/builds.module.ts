a558476fe4a2a391ef32c99a19d91201fc09085c
Extacy <git@extacy.cc>
2021-06-08 08:54:06 +0000

    Revert "Remove AuthKeyGuard, enable JwtGuard for builds"
    This reverts commit 937d3c5efcd51891288811ce70165bd236d2c2e4.

--- container_data/api/src/builds/builds.module.ts
+++ container_data/api/src/builds/builds.module.ts
@@ -1,8 +1,6 @@
 
 import { Module } from "@nestjs/common";
-import { APP_GUARD } from "@nestjs/core";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
 import { BuildsController } from "./builds.controller";
 import { BuildsService } from "./builds.service";
 import { Build } from "./models/build.entity";
@@ -10,12 +8,6 @@ import { Build } from "./models/build.entity";
 @Module({
     imports: [TypeOrmModule.forFeature([Build])],
     controllers: [BuildsController],
-    providers: [
-        BuildsService,
-        {
-            provide: APP_GUARD,
-            useClass: JwtAuthGuard
-        }
-    ],
+    providers: [BuildsService],
 })
 export class BuildsModule { }
