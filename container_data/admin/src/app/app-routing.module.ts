83192e973684363bc1a16dacb4b0900cf528beaf
Extacy <git@extacy.cc>
2021-06-16 03:04:10 +0000

    Add basic pages/routes + bootstrap

--- container_data/admin/src/app/app-routing.module.ts
+++ container_data/admin/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AdminComponent } from "./components/admin/admin.component";
+import { BuildsComponent } from "./components/admin/builds/builds.component";
+import { HomeComponent } from "./components/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: "", redirectTo: "home", pathMatch: "full"},
+    { path: "home", component: HomeComponent},
+    {
+        path: "admin", component: AdminComponent,
+        children: [
+            { path: "builds", component: BuildsComponent },
+        ]
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- container_data/admin/src/app/app.module.ts
+++ container_data/admin/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { HomeComponent } from "./components/home/home.component";
+import { AdminComponent } from "./components/admin/admin.component";
+import { BuildsComponent } from "./components/admin/builds/builds.component";
+import { HeaderComponent } from "./components/admin/header/header.component";
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        AdminComponent,
+        BuildsComponent,
+        HeaderComponent
     ],
     imports: [
         BrowserModule,
