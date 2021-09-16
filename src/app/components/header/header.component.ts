7ace0d9a1b65f7913bde1490468f491681784c55
Extacy <git@extacy.cc>
2021-09-17 00:23:30 +1200

    Only show admin section if user is an admin

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,7 +11,7 @@ export class HeaderComponent implements OnInit {
     @Input()
     public title: string;
 
-    constructor(private authService: AuthService) { }
+    constructor(public authService: AuthService) { }
 
     public ngOnInit(): void {
     }
